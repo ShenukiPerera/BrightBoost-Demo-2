b83dd57aef965b78c17b0aa77a7105a5f8323db0
Vishwanath Alagappan <alvishwanath27@gmail.com>
2023-10-27 17:59:56 +1100

    updated student

--- Student/join_queue.php
+++ Student/join_queue.php
@@ -50,7 +50,14 @@ if ($result_next_session->num_rows > 0) {
         // Insert the student's request into the queue
         $sql_insert_queue = "INSERT INTO queue (sessionid, studentid) VALUES ('$sessionid', '$studentid')";
         if ($conn->query($sql_insert_queue) === TRUE) {
+            // Display the question form
             echo "You have successfully joined the session queue and can now ask questions!";
+            echo "<h3>Ask a Question:</h3>";
+            echo "<form action='submit_question.php' method='post'>";
+            echo "<input type='hidden' name='sessionid' value='$sessionid'>";
+            echo "<textarea name='question' rows='4' cols='50' placeholder='Type your question here'></textarea><br>";
+            echo "<input type='submit' value='Submit Question'>";
+            echo "</form>";
         } else {
             echo "Error: " . $sql_insert_queue . "<br>" . $conn->error;
         }
